fe553debcf254062ba7ba49ca82bc66523c7ad2d
Bertasso778 <pedrinff036@gmail.com>
2024-10-09 08:46:41 -0300

    inicio flappy bird

---
+++ Flapybird/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Flapybird;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
