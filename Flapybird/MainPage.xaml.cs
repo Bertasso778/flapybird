cbcde76521fb5910b53a2580ad56c5cb770b5cdd
Bertasso778 <pedrinff036@gmail.com>
2024-10-11 08:58:58 -0300

    atualização

--- Flapybird/MainPage.xaml.cs
+++ Flapybird/MainPage.xaml.cs
@@ -1,24 +1,67 @@
-﻿namespace Flapybird;
+﻿using System.Net.Security;
+
+namespace Flapybird;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+	const int Gravidade = 2;
+	const int tempoEntreFrames = 25;
+	bool estaMorto = true;
+	double LarguraJanela = 0;
+	double AlturaJanela = 0;
+	int Velocidade =20;
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	void AplicaGravidade()
 	{
-		count++;
+		imgPersonagem.TranslationY += Gravidade;
+	}
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+	async Task Desenha()
+	{
+		while (!estaMorto)
+		{
+			AplicaGravidade();
+			await Task.Delay(tempoEntreFrames);
+		}
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+
+	void OnGameOverClicked(object s, TappedEventArgs e)
+	{
+		frameGameOver.IsVisible = false;
+		Inicializar();
+		Desenha();
+
+	}
+
+	void Inicializar()
+	{
+		estaMorto = false;
+		imgPersonagem.TranslationY = 0;
+	}
+    protected override void OnSizeAllocated(double width, double height)
+    {
+        base.OnSizeAllocated(width, height);
+		
+		LarguraJanela = width;
+		
+		AlturaJanela =height;
+    }
+
+	void GerenciaCanos()
+	{
+ 		posteCima.TranslationX -= Velocidade;
+	posteBaixo.TranslationX -= Velocidade;
+	if(posteBaixo.TranslationX < -LarguraJanela)
+	{
+		posteBaixo.TranslationX = 200;
+		posteBaixo.TranslationX = 200;
+	}
 	}
 }
 
